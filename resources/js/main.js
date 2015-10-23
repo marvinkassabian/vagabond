d152900acec85fe29885f344bf3e5a552e6f9f64
Marvin Lincoln Kassabian <marvin.kassabian@gmail.com>
2015-10-23 10:31:52 -0400

    Hack in polymorphism without new

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,18 +1,26 @@
 (function() {
 
-  var Entity = function() {
+  var Entity = (function() {
     var x;
     var y;
 
     var Entity = {};
 
     Entity.init = function(x0, y0) {
-      setCoor(x, y);
+      return Entity.initEntity(x0, y0);
+    };
+
+    Entity.initEntity = function(x0, y0) {
+      this.setCoor(x0, y0);
+
+      return this;
     };
 
     Entity.setCoor = function(x0, y0) {
       x = x0;
       y = y0;
+
+      return this;
     };
 
     Entity.getCoor = function() {
@@ -25,19 +33,27 @@
     Entity.move = function(dx, dy) {
       x += dx;
       y += dy;
+
+      return this;
     };
 
     return Entity;
-  };
+  })();
 
-  var KillableEntity = function() {
+  var KillableEntity = (function() {
     var hitPoints;
 
-    var KillableEntity = Object.create(Entity());
+    var KillableEntity = Object.create(Entity);
 
     KillableEntity.init = function(x0, y0, hp0) {
-      this.setCoor(x0, y0);
+      return this.initKillableEntity(x0, y0, hp0);
+    };
+
+    KillableEntity.initKillableEntity = function(x0, y0, hp0) {
+      this.initEntity(x0, y0);
       this.setHealth(hp0);
+
+      return this;
     };
 
     KillableEntity.setHealth = function(hp) {
@@ -49,19 +65,24 @@
     };
 
     return KillableEntity;
-  };
+  })();
 
-  var Monster = function() {
+  var Monster = (function() {
     var attack;
     var defense;
 
-    var Monster = Object.create(KillableEntity());
+    var Monster = Object.create(KillableEntity);
 
     Monster.init = function(x0, y0, hp0, a0, d0) {
-      this.setCoor(x0, y0);
-      this.setHealth(hp0);
+      return this.initMonster(x0, y0, hp0, a0, d0);
+    };
+
+    Monster.initMonster = function(x0, y0, hp0, a0, d0) {
+      this.initKillableEntity(x0, y0, hp0);
       this.setAttack(a0);
       this.setDefense(d0);
+
+      return this;
     };
 
     Monster.setAttack = function(a0) {
@@ -87,17 +108,13 @@
     };
 
     return Monster;
-  };
+  })();
 
-  var milo = Object.create(Monster());
-
-  milo.init(2, 5, 30, 4, 6);
+  var milo = Object.create(Monster).init(2, 5, 30, 4, 6);
 
   console.log(milo.getCoor());
   console.log(milo.getHealth());
   console.log(milo.getDefense());
   milo.takeDamage(10);
   console.log(milo.getHealth());
-  console.log(milo);
-
 })();
