dad3eac1cd7cb2f75ad4549799eaa6cba71ea00c
Marvin Lincoln Kassabian <marvin.kassabian@gmail.com>
2015-11-10 13:40:47 -0500

    Grunt now creates files for marvinkassabian.github.io

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,45 +1,49 @@
+// jscs:disable
 module.exports = function(grunt) {
   "use strict";
+
+  var utilFiles = [
+    "resources/js/util/util.js",
+    "resources/js/util/virtualkeys.js"
+  ];
+  var vagabondFiles = [
+    "resources/js/vagabond.js",
+    "resources/js/matrix.js",
+    "resources/js/algorithms.js",
+    "resources/js/maps/mapmodes.js",
+    "resources/js/maps/map.js",
+    "resources/js/entities/entity.js",
+    "resources/js/entities/killableentity.js",
+    "resources/js/entities/movablekillableentity.js",
+    "resources/js/entities/monster.js",
+    "resources/js/entities/enemies/goblin.js",
+    "resources/js/maps/factory.js",
+    "resources/js/controls/listener.js",
+    "resources/js/controls/controller.js",
+    "resources/js/screen.js",
+    "resources/js/level.js",
+    "resources/js/main.js"
+  ];
 
   // Project configuration.
   grunt.initConfig({
     uglify: {
+      options: {
+        sourceMap: true,
+        sourceMapName: function(path) {
+          return path + ".map";
+        }
+      },
       util: {
-        options: {
-          sourceMap: true,
-          sourceMapName: "util.min.js.map"
-        },
         files: {
-          "util.min.js": [
-            "resources/js/util/util.js",
-            "resources/js/util/virtualkeys.js"
-          ]
+          "util.min.js": utilFiles,
+          "../marvinkassabian.github.io/util.min.js": utilFiles
         }
       },
       vagabond: {
-        options: {
-          sourceMap: true,
-          sourceMapName: "vagabond.min.js.map"
-        },
         files: {
-          "vagabond.min.js": [
-            "resources/js/vagabond.js",
-            "resources/js/matrix.js",
-            "resources/js/algorithms.js",
-            "resources/js/maps/mapmodes.js",
-            "resources/js/maps/map.js",
-            "resources/js/entities/entity.js",
-            "resources/js/entities/killableentity.js",
-            "resources/js/entities/movablekillableentity.js",
-            "resources/js/entities/monster.js",
-            "resources/js/entities/enemies/goblin.js",
-            "resources/js/maps/factory.js",
-            "resources/js/controls/listener.js",
-            "resources/js/controls/controller.js",
-            "resources/js/screen.js",
-            "resources/js/level.js",
-            "resources/js/main.js"
-          ]
+          "vagabond.min.js": vagabondFiles,
+          "../marvinkassabian.github.io/vagabond.min.js": vagabondFiles
         }
       }
     }
