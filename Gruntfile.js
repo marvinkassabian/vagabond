e0d2462968cadc34029a4246c612a4adeb1957c2
Marvin Lincoln Kassabian <marvin.kassabian@gmail.com>
2015-11-20 10:54:45 +0000

    Change module load order

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,17 +4,19 @@ module.exports = function(grunt) {
 
   var utilFiles = [
     "resources/js/util/util.js",
-    "resources/js/util/virtualkeys.js"
+    "resources/js/util/virtualkeys.js",
   ];
   var vagabondFiles = [
     "resources/js/vagabond.js",
     "resources/js/datastructures/matrix.js",
     "resources/js/datastructures/graph.js",
+    "resources/js/view/view.js",
+    "resources/js/view/logger.js",
+    "resources/js/view/information.js",
+    "resources/js/view/screen.js",
     "resources/js/algorithms/astar.js",
     "resources/js/algorithms/cellularautomata.js",
     "resources/js/algorithms/diamondsquare.js",
-    "resources/js/view/view.js",
-    "resources/js/view/logger.js",
     "resources/js/model/maps/map.js",
     "resources/js/model/maps/factory.js",
     "resources/js/model/entities/entity.js",
@@ -23,43 +25,42 @@ module.exports = function(grunt) {
     "resources/js/model/entities/monster.js",
     "resources/js/model/entities/enemies/goblin.js",
     "resources/js/model/entities/playerentity.js",
-    "resources/js/view/information.js",
+    "resources/js/model/level.js",
     "resources/js/controller/eventstack.js",
     "resources/js/controller/keylistener.js",
     "resources/js/controller/maplistener.js",
     "resources/js/controller/controller.js",
-    "resources/js/view/screen.js",
-    "resources/js/model/level.js"
   ];
 
   var mainFile = [
-    "resources/js/main.js"
+    "resources/js/main.js",
   ];
 
   var mainOutputs = {
     "compiled/js/main.min.js": mainFile,
-    "../marvinkassabian.github.io/vagabond/main.min.js": mainFile
+    "../marvinkassabian.github.io/vagabond/main.min.js": mainFile,
   };
 
   var utilOutputs = {
     "compiled/js/util.min.js": utilFiles,
-    "../marvinkassabian.github.io/vagabond/util.min.js": utilFiles
+    "../marvinkassabian.github.io/vagabond/util.min.js": utilFiles,
   };
+
   var vagabondOutputs = {
     "compiled/js/vagabond.min.js": vagabondFiles,
-    "../marvinkassabian.github.io/vagabond/vagabond.min.js": vagabondFiles
+    "../marvinkassabian.github.io/vagabond/vagabond.min.js": vagabondFiles,
   };
 
   var debugOff = {
     global_defs: {
       "DEBUG": false
     },
-    dead_code: true
+    dead_code: true,
   };
 
   var debugOn = {
     global_defs: {
-      "DEBUG": true
+      "DEBUG": true,
     }
   };
 
@@ -71,22 +72,22 @@ module.exports = function(grunt) {
         sourceMapName: function(path) {
           return path + ".map";
         },
-        compress: debugOff
+        compress: debugOff,
       },
       util: {
-        files: utilOutputs
+        files: utilOutputs,
       },
       vagabond: {
-        files: vagabondOutputs
+        files: vagabondOutputs,
       },
       vagabond_DEBUG: {
         files: vagabondOutputs,
         options: {
-          compress: debugOn
+          compress: debugOn,
         }
       },
       main: {
-        files: mainOutputs
+        files: mainOutputs,
       }
     }
   });
